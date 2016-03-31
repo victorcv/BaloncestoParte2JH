69fd5cc17a016bd279f5d80aea9b5fb27422ba4c
alfredoru <alfredoru@gmail.com>
2016-03-31 20:21:24 +0200

    jgg

--- src/main/webapp/controlCanastasTotales.ctrl.js
+++ src/main/webapp/controlCanastasTotales.ctrl.js
@@ -5,7 +5,6 @@
 'use strict'
 angular.module('ligaBaloncestoApp')
     .controller('VicCtrl',function ($scope, $http) {
-
        //$http.get("api/jugadorsURL/70000").then(function(response){$scope.jugadores=response.data;});// response es response.data(la respuesta de la peticion)
         // peticion get a la url del request maping(en el JugadorResource). la respuesta de la funcion la guardamos en el scope de jugadores para hacer el ng.repeat
         //$scope.jugadores hace referencia al model jugadores(lo hemos llamado asi)
